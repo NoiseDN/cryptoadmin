53098e214f6e1f5a36311cd2ae82997f7e99149f
Anton Filimonov <Anton.Filimonov@get.no>
2018-05-04 14:53:23 +0300

    Fix admin config

---
+++ src/main/java/com/melafon/cryptoadmin/PropertySourceConfig.java
@@ -0,0 +1,16 @@
+package com.melafon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("file:/opt/cryptoscan.properties")
+public class PropertySourceConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
